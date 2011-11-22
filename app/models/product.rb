35b95d99288815dddf9e0c84c71e442d96dc93dc
stephen farrell <stephenfarrell8@gmail.com>
2011-11-22 14:28:20 +0000

    Initial commit

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ActiveRecord::Base
+  belongs_to :user
+    has_many :line_items
+  def self.search(search_query)
+    if search_query
+      find(:all, :conditions => ['name LIKE ?', "%#{ search_query}%"])
+    else
+      find(:all)
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaptopShop1::Application.config.secret_token = 'ffb9f060f01eac3d0177667bdeeba2a1199b19bab67edfe86efcb6a4510b1a1a7215c3c06c79f2bc04e7d3ebb5e0b1d46223af276e327b7ff754651a295f43fd'
